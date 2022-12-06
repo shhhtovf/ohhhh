02c512dcd5474cb9721dd12494194163e92d4125
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2022-12-06 18:47:16 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-  loadstring(game:HttpGet("https://jjt-x-hub.jjtchannel.repl.co/games/BF.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/ohhhh/main/games/BF.lua"))()
 elseif PlaceId == 9872472334 then
-  loadstring(game:HttpGet("https://jjt-x-hub.jjtchannel.repl.co/games/evade.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/ohhhh/main/games/evade.lua"))()
 else
   game.StarterGui:SetCore("SendNotification", {
       Icon = "rbxassetid://10845671105";
