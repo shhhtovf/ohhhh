2c27b514f631f2271b05a5f91ab2226824acb13a
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2022-12-10 18:55:22 +0700

    Update checkkey.lua

--- checkkey.lua
+++ checkkey.lua
@@ -3,10 +3,7 @@ local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
 local id = 107868627 -- Put your id
 local plr = game.Players.LocalPlayer.UserId
 
-if _G.key == "ADMIN" then
-  print("Correct Admin")
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/shhhtovf/ohhhh/main/loader.lua")()
-elseif _G.key == "JJT_" .. hwid then
+if _G.key == "JJT_" .. hwid then
   print("Correct")
   loadstring(game:HttpGet"https://raw.githubusercontent.com/shhhtovf/ohhhh/main/loader.lua")()
 elseif game:GetService("MarketplaceService"):UserOwnsGamePassAsync(plr, id) then
