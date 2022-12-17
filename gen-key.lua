c15d17a9d13001b1adfefe326d515869cabe548d
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2022-12-17 19:03:27 +0700

    Update gen-key.lua

--- gen-key.lua
+++ gen-key.lua
@@ -1,7 +1,1 @@
-local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
-
-local plr = game.Players.LocalPlayers
-
-
-
 setclipboard("JJT-" .. game:GetService("RbxAnalyticsService"):GetClientId(),game.Players.LocalPlayer,game.Players.LocalPlayer.UserId)
