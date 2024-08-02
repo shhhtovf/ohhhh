6a277d45265fdf953422eb41963ca16563285030
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2024-08-02 15:21:09 +0700

    Update checkkey.lua

--- checkkey.lua
+++ checkkey.lua
@@ -1,13 +1,14 @@
 local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
 
-if _G.key == "JJTxHub" .. hwid then
+if _G.key == "JJTxHub-" .. hwid then
   print("Correct")
   loadstring(game:HttpGet"https://raw.githubusercontent.com/shhhtovf/ohhhh/main/loader.lua")()
 else
   print("incorrect KEY!")
   game.StarterGui:SetCore("SendNotification", {
 Title = "JJTxHub"; -- the title 
-Text = "Wrong Key :("; -- what the text says 
+Text = "FREE KEY!!!"; -- what the text says 
 Duration = 5; -- how long the notification should in secounds
 })
+  loadstring(game:HttpGet"https://raw.githubusercontent.com/shhhtovf/ohhhh/main/loader.lua")()
 end
